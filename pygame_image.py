0cd451b00784695068d1e454c8cbc6abc225a1d4
Shogo <c0a231346d@edu.teu.ac.jp>
2024-04-16 17:40:33 +0900

    課題１

--- pygame_image.py
+++ pygame_image.py
@@ -28,6 +28,7 @@ def main():
             kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((+2,0))
+        kk_rct.move_ip((-1,0))
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
