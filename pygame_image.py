ad5f71b1e942e617e3f6b45857165d9740cdf1c0
Shogo <c0a231346d@edu.teu.ac.jp>
2024-04-16 17:27:38 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -11,20 +11,30 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_ton = pg.image.load("fig/3.png")
-    kk_ton = pg.transform.flip(kk_ton,True,False) 
+    kk_ton = pg.transform.flip(kk_ton,True,False)
+    kk_rct = kk_ton.get_rect()
+    kk_rct.center = 300,200
     tmr = 0
     surface = pg.transform.flip(bg_img,True,False)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,+1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1,0))
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(surface,[-x+1600,0])
         screen.blit(bg_img,[-x+3200,0])
         screen.blit(surface,[-x+4800,0])
-        screen.blit(kk_ton,[300,200])
+        screen.blit(kk_ton,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
